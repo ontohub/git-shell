d55e849c9ab8f48bdb2060c422ff0d7b78c2b9cf
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-02-16 22:56:12 +0100

    Fix Travis.

--- bin/git-shell-travis.sh
+++ bin/git-shell-travis.sh
@@ -7,5 +7,6 @@ cd $dir/..
 # Get this path with `rvm $(cat .ruby-version) do rvm env --path`:
 source /home/travis/.rvm/environments/ruby-$(cat .ruby-version)
 
+export BUNDLE_GEMFILE="$(readlink -f Gemfile)"
 # Replace this process to propagate the exit code
 exec bundle exec bin/git-shell "$@"
